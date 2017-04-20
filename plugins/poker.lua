b8310ee8a632c6062e8d7c8cb192b5c0962f99b0
SorBlack <sorblackplus@gmail.com>
2017-04-20 18:46:02 +0430

    Add Plugin Poker By @SorBlack :):|

---
+++ plugins/poker.lua
@@ -0,0 +1,20 @@
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+local text = '😐'
+if matches[1] == '😐' then
+    if is_sudo(msg) then
+return text
+end
+end 
+end
+return {
+patterns = {
+    "😐"
+},
+run = run
+}
+
+end
+-- by #sorblack :):|
