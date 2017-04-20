e898cf86537cc09ce6e8b4ffadfe75c3f79f5b94
SorBlack <sorblackplus@gmail.com>
2017-04-20 18:53:55 +0430

    Add Plugin Edit By @SorBlack :):|

---
+++ plugins/edit.lua
@@ -0,0 +1,23 @@
+local function m(msg, matches)
+if matches[1] == 'edit' and msg.reply_to_message_id_ ~= 0 and is_sudo(msg) then
+local text = matches[2]
+tdcli.editMessageText(msg.to.id, msg.reply_to_message_id_, nil, text, 1, 'md')
+end
+--//////-//Edit Cap-----//+++++
+if matches[1] == 'edit' and msg.reply_to_message_id_ ~= 0 and is_sudo(msg) then
+local tExt = matches[2]
+tdcli.editMessageCaption(msg.to.id, msg.reply_to_message_id_, nil, tExt)
+end
+end
+return {
+patterns = { 
+"^[!#/](edit) (.*)$",
+"^[!#/](edit) (.*)$",
+"^(اصلاح) (.*)$",
+"^(اصلاح) (.*)$",
+},
+run = m,
+}
+
+-- by #sorblack :)
+-- channel: @PrimeTeam
