a7d46673e02b90464a7c2b0e7e65338cafeb4fe1
SorBlack <sorblackplus@gmail.com>
2017-04-20 18:10:34 +0430

    Add Plugin Rank By @SorBlack :):|

---
+++ plugins/rank.lua
@@ -0,0 +1,25 @@
+do
+
+local function run(msg, matches)
+if matches[1]=="مقام من" and is_sudo(msg) then 
+return  "شما صاحب ربات و سودو میباشید😐❤️"
+elseif matches[1]=="مقام من" and is_admin(msg) then 
+return  "شما ادمین ربات و یک مقام پایین تر ازسودو میباشید😐💋"
+elseif matches[1]=="مقام من" and is_owner(msg) then 
+return  "شما سازنده و صاحب گروه میباشید😐🎈"
+elseif matches[1]=="مقام من" and is_mod(msg) then 
+return  "شما کمک مدیراین گروه میباشید😐🦃"
+else
+return  "شما هیچ مقامی در ربات ندارید😐🖕🏿"
+end
+
+end
+
+return {
+  patterns = {
+    "^(مقام من)$",
+    },
+  run = run
+}
+end
+-- By #SorBlack :):|
